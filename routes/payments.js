82c15fd682e7a53cf0c09e023abf2c8f172ca280
Rahul Gaur <gaur.rhl91@gmail.com>
2015-02-05 16:39:36 -0500

    Update payments.js

--- routes/payments.js
+++ routes/payments.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+function throw_err(err, res) {
+    res.json({ 'error': {
+        message: err.message,
+        error: err
+    }});
+    throw err;
+}
+
 /* GET payment listing. */
 
 projection = function(req,res){
@@ -13,6 +21,18 @@ router.get('/', function(req, res){
 	else
 		return pagination(req, res, '*', 'payment');
 });
+
+
+router.get('/:id', function(req, res) {	
+	var db = req.sql;	
+	sql.query('SELECT * FROM payment WHERE payment_id=' + req.params.id, function(err,rows,fields) {
+	if(err) throw_err(err,res);
+	res.json({'store': rows});
+	});
+
+});
 //e.g http://localhost:3000/payments?offset=0&limit=10
 
 module.exports = router;
+
+
