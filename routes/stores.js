45673765f3092be2242f0b03ac944694b44906d3
Rahul Gaur <gaur.rhl91@gmail.com>
2015-02-05 15:40:36 -0500

    Update stores.js
    Added the correct queries for get, put and post. 
    Yet to experiment on Post. 
    Need to experiment with the change of Foreign Key.

--- routes/stores.js
+++ routes/stores.js
@@ -1,16 +1,46 @@
+
 var express = require('express');
 var router = express.Router();
 
+function throw_err(err, res) {
+    res.json({ 'error': {
+        message: err.message,
+        error: err
+    }});
+    throw err;
+}
+
 /* GET store listing. */
 
-router.get('/', function(req, res, next) {
-	sql.query('SELECT * FROM store WHERE store_id=' + req.query.id +
-	' AND address_id=' + req.query.addr)
-	.then(function(query_res) {
-	res.send(query_res);
+//ran on localhost:3000/stores/1 
+router.get('/:id', function(req, res) {	
+	var db = req.sql;	
+	sql.query('SELECT * FROM store WHERE store_id=' + req.params.id, function(err,rows,fields) {
+	if(err) throw_err(err,res);
+	res.json({'store': rows});
 	});
+
 });
 
-//e.g http://localhost:3000/stores?offset=0&limit=10
+/*/Post
+router.post('/', function(req, res) {
+    query = 'INSERT INTO store (store_id,manager_staff_id,address_id,last_update) VALUES (?,?,?)';
+    params = [req.body.store_id, req.body.manager_staff_id,req.body.address_id,req.body.last_update]
+    sql.query(query, params, function(err, rows, fields) {
+        if (err) throw_err(err, res);
+        res.json({ 'success': 1 });
+    });
+});
 
-module.exports = router;
+/*
+ * PUT stores/id.
+ */
+// localhost:3000/stores/1
+router.put('/:id', function(req, res) {
+    query = 'UPDATE store SET last_update= ? WHERE store_id = ?';
+    params = [req.body.last_update,req.params.id]
+    sql.query(query, params, function(err, rows, fields) {
+        if (err) throw_err(err, res);
+        res.json({ 'success': 1 });
+    });
+});
